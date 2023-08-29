8740af873fd3df93c05039ee022f8930674f1d47
Bbotev <dark_terror_90@abv.bg>
2023-08-29 13:13:49 +0300

    done

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -73,7 +73,7 @@ router.post("/orders",async(req,res)=>{
         name:check.firstName,
         status:1,
         date: date.getFullYear()+'-'+("0"+(date.getMonth()+1)).slice(-2)+'-'+
-        ("0"+(date.getDate())).slice(-2)+'-'+date.getHours()+':'+("0"+(date.getMinutes())).slice(-2),
+        ("0"+(date.getDate())).slice(-2)+' '+date.getHours()+':'+("0"+(date.getMinutes())).slice(-2),
         email:check.email,
         phone:check.phone,
         products:orderText,
